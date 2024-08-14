2def026510d331a9a706d45280eb81100c715747
Sitanshu Hallad <83655509+sitanshu05@users.noreply.github.com>
2024-08-14 17:42:04 +0530

    Full blog made responsive

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -8,7 +8,7 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
         <AppBar />
         <div className="flex justify-center">
             <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
-                <div className="col-span-7">
+                <div className=" col-span-12 md:col-span-7">
                     <div className="text-5xl font-extrabold">
                         {blog.title}
                     </div>
@@ -19,8 +19,8 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
                         {blog.content}
                     </div>
                 </div>
-                <div className="cols-span-1"></div>
-                <div className="col-span-4">
+                <div className="hidden md:block md:cols-span-1"></div>
+                <div className="hidden md:block md:col-span-4">
                     <div className="text-slate-600 text-lg">
                         Author
                     </div>
@@ -37,9 +37,27 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
                             </div>
                         </div>
                     </div>  
-                </div>
-                
+                </div> 
+                <div className="md:hidden w-full col-span-12  pt-2 mt-5 border-t-2 border-slate-200">
+                    <div className="text-slate-600 text-lg ">
+                        Author
+                    </div>
+                    <div className="flex w-full">
+                        <div className="pr-4 flex flex-col justify-center">
+                            <Avatar size="big" name={blog.author.name || "Anonymous"} />
+                        </div>
+                        <div>
+                            <div className="text-xl font-bold">
+                                {blog.author.name || "Anonymous"}
+                            </div>
+                            <div className="pt-2 text-slate-500">
+                                Random catch phrase about the author's ability to grab the user's attention
+                            </div>
+                        </div>
+                    </div>  
+                </div> 
             </div>
         </div>
+       
     </div>
 }
